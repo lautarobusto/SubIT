544da8296eafea4f54313b0d6703f6a0ee226e7f
lautarobusto <lautarobusto@gmail.com>
2016-11-09 01:11:25 -0300

    intento de almacenamiento en una lista de objetos

--- main.py
+++ main.py
@@ -10,17 +10,14 @@ url3 = "http://www.subdivx.com/index.php?buscar=game+of+thrones+s06e01&accion=5&
 # Realizamos la petición a la web
 r = requests.get(url3)
 statusCode = r.status_code
-linkList=[]
-tituloList=[]
-descripcionList=[]
-subtitulos=[]
-subtitulo =Subtitulo
+linkList = []
+tituloList = []
+descripcionList = []
+subtitulos = []
+
 if statusCode == 200:
 
     html = BeautifulSoup(r.text, 'html.parser')
-
-    descripciones = html.find_all('div', id="buscador_detalle_sub")
-    divLinks = html.find_all('div', id="buscador_detalle_sub_datos")
 
     for titulo in html.find_all('div', id="menu_titulo_buscador"):
         tituloList.append(titulo.find('a').text)
@@ -29,19 +26,27 @@ if statusCode == 200:
     for link in html.find_all('a', target="new", rel="nofollow"):
         linkList.append(link.get('href'))
 
-    for i in xrange(linkList.len):
-        subtitulo.titulo = tituloList[i]
-        subtitulo.descripcion = descripcionList[i]
-        subtitulo.link = linkList[i]
+
+     ##Si esto funciona porque al meterlo dentro de  un for, todos los objetos tienen el mismo contenido?
+    sub=Subtitulo
+    sub.titulo=tituloList[0]
+    sub.descripcion=descripcionList[0]
+    sub.link=linkList=linkList[0]
+    subtitulos.append(sub)
+
+    print(subtitulos[0].titulo)
+    print(subtitulos[0].descripcion)
+    print(subtitulos[0].link)
 
 
+    #for index in range(0, len(linkList)):
+       # sub = Subtitulo
 
-
-
+       # sub.titulo = tituloList[index]
+       # sub.descripcion = descripcionList[index]
+       # sub.link = linkList[index]
+       # subtitulos.append(sub)
 
 else:
-    print("Status Code %d" % statusCode)
 
-    # <a rel="nofollow" target="new" href="http://www.subdivx.com/bajar.php?id=247551&amp;u=7"><img src="bajar_sub.gif" border="0"></a>
-    # http://www.subdivx.com/bajar.php?id=7413&u=1
-    # http://www.subdivx.com/bajar.php?id=7413&amp;u=1
+    print("Status Code %d" % statusCode)
