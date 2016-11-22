d639e4271953d15d0ea6d10a288305adce39c813
lautarobusto <lautarobusto@gmail.com>
2016-11-22 06:43:56 -0300

    proceso listo, empiezo a trabajar la GUI

--- main.py
+++ main.py
@@ -2,81 +2,94 @@ from bs4 import BeautifulSoup
 import requests
 from Subtitulo import Subtitulo
 import re
+import codecs
 
-# url de subdivx con una buscqueda ingresada en el campo de busqueda de subdivx
-url = "http://www.subdivx.com/index.php?buscar=ironman&accion=5&masdesc=&subtitulos=1&realiza_b=1"
 
-linkList = []
-tituloList = []
-descripcionList = []
-descargaList = []
-subtitulos = []
-cantP = []
-nextList = []
-hasnext = True
-cant = 0
+def getsubs(url):
+    linkList = []
+    tituloList = []
+    descripcionList = []
+    descargaList = []
+    subtitulos = []
+    cantP = []
+    nextList = []
+    hasnext = True
+    cant = 0
 
-while hasnext == True:
+    while hasnext is True:
 
-    r = requests.get(url)
-    statusCode = r.status_code
-    if statusCode == 200:
+        r = requests.get(url)
+        statusCode = r.status_code
+        if statusCode == 200:
 
-        html = BeautifulSoup(r.text, 'html.parser')
+            html = BeautifulSoup(r.text, 'html.parser')
 
-        # Checkeo si hay mas paginas y obtengo enlace de la proxima si existe
-        pag = html.find('div', {"class": "pagination"})
-        # print(type(pag))
+            # Checkeo si hay mas paginas y obtengo enlace de la proxima si existe
+            pag = html.find('div', {"class": "pagination"})
+            # print(type(pag))
 
-        if len(pag) > 0:
-            current = pag.find('span', {"class": "current"}).text
+            if len(pag) > 0:
+                current = pag.find('span', {"class": "current"}).text
 
-            # con esto obtengo la cantidad de paginas que me devolvio la consulta inicial
-            for a in pag.find_all('a'):
-                cantP.append(a.text)
-                nextList.append("http://www.subdivx.com/" + a.get("href"))
-            if cant == 0:
-                cant = cantP[-2]
-            url = nextList[-1]
+                # con esto obtengo la cantidad de paginas que me devolvio la consulta inicial
+                for a in pag.find_all('a'):
+                    cantP.append(a.text)
+                    nextList.append("http://www.subdivx.com/" + a.get("href"))
+                if cant == 0:
+                    cant = cantP[-2]
+                url = nextList[-1]
 
-            if int(current) == int(cant):
+                if int(current) == int(cant):
+                    hasnext = False
+
+            else:
                 hasnext = False
 
+            # obtengo Titulo, descripcion, descargas y enlaces por cada subtitulo y lo almaceno en una lista de objetos Subtitulo
+            for titulo in html.find_all('div', id="menu_titulo_buscador"):
+                tituloList.append(titulo.find('a').text)
+            for descripcion in html.find_all('div', id="buscador_detalle_sub"):
+                descripcionList.append(descripcion.text)
+            for link in html.find_all('a', target="new", rel="nofollow"):
+                linkList.append(link.get('href'))
+            for descarga in html.find_all('div', id="buscador_detalle_sub_datos"):
+                wordList = re.sub("[^0-9,]", " ", descarga.text).split()
+                descargaList.append(wordList[0])
+
+            for i in range(len(tituloList)):
+                x = Subtitulo(tituloList[i], descripcionList[i], descargaList[i], linkList[i])
+
+                subtitulos.append(x)
         else:
 
-            hasnext = False
-
-        # obtengo Titulo, descripcion, descargas y enlaces por cada subtitulo y lo almaceno en una lista de objetos Subtitulo
-        for titulo in html.find_all('div', id="menu_titulo_buscador"):
-            tituloList.append(titulo.find('a').text)
-        for descripcion in html.find_all('div', id="buscador_detalle_sub"):
-            descripcionList.append(descripcion.text)
-        for link in html.find_all('a', target="new", rel="nofollow"):
-            linkList.append(link.get('href'))
-
-        for descarga in html.find_all('div', id="buscador_detalle_sub_datos"):
-            wordList = re.sub("[^0-9\\,]", " ", descarga.text).split()
-            descargaList.append(wordList[0])
-
-        for i in range(len(tituloList)):
-            x = Subtitulo(tituloList[i], descripcionList[i], descargaList[i], linkList[i])
-
-            subtitulos.append(x)
+            print("Status Code %d" % statusCode)
+    return subtitulos
 
 
+def geturl(title, season, chapter):
+    title = "+".join(title.split())
 
+    if len(season) == 0 | len(chapter) == 0:
 
-
-
-
-
+        url = "http://www.subdivx.com/index.php?accion=5&masdesc=&buscar=" + title + "&oxdown=1"
     else:
 
-        print("Status Code %d" % statusCode)
-f = open('file.txt', 'w')
+        url = "http://www.subdivx.com/index.php?accion=5&buscar=" + title + "+s" + season + "e" + chapter + "&oxdown=1"
+    return url
 
-for subs in subtitulos:
-    f.write(
-        "--------------------------------------------------------------------------------------------------------------------------------------------\n")
-    f.write("Titulo:" + subs.titulo + "\n" + "Descargas:" + subs.descargas + "\n" + "Descripcion:" + str(
-        subs.descripcion.encode('utf8')) + "\n" + "Enlace:" + subs.link + "\n")
+
+
+
+
+def saveresultfile():
+
+    subtitulos = getsubs(geturl("ironman", "", ""))
+
+    f = codecs.open('file.txt', 'w', "utf-8")
+    for subs in subtitulos:
+        f.write(
+            "--------------------------------------------------------------------------------------------------------------------------------------------\n")
+        f.write("Titulo:" + subs.titulo + "\n" + "Descargas:" + subs.descargas + "\n" + "Descripcion:" +
+                subs.descripcion + "\n" + "Enlace:" + subs.link + "\n")
+
+saveresultfile()
